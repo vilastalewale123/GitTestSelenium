2dc98ec6ba3f7df868668470c0150fb331b61646
Admin <Admin@192.168.0.5>
2021-04-10 21:57:26 +0530

    Initial commit

---
+++ GitTest/src/test/java/com/vilas/TestGit.java
@@ -0,0 +1,31 @@
+package com.vilas;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestGit {
+public WebDriver driver;
+	
+	@Test
+	public void vilastutorials() {
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(45,
+				TimeUnit.SECONDS);
+		String title = "Meet Guru99";
+		String altTitle = driver.getTitle();
+		if(altTitle.contains(title)){
+			System.out.println("Test case is passed");
+		}else {
+			System.out.println("Test case is passed");
+		}
+		driver.close();
+		
+	}
+
+}
